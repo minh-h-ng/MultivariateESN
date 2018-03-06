dcfc9f7d18b48165a243cd804005a0adaacb0b43
Hoang Minh Nguyen <minhhoang@kaist.ac.kr>
2018-03-06 16:31:52 +0900

    initialize project

---
+++ run_single_experiment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# NOTE: This file needs to be executable (i.e. chmod +x run_experiment.sh before attempting to run)
+
+DATAFILE=$1
+OPTCONFIG=$2
+ESNCONFIG=$3
+RUNS=$4
+
+#DATAFILE=./data/NARMA
+#OPTCONFIG=ridge_identity
+#ESNCONFIG=esnconfig
+#RUNS=30
+
+# Tune parameters. Note: the config file for the best parameters are saved at the location in $ESNCONFIG
+python -m scoop -n 8 ./genoptesn.py $DATAFILE $OPTCONFIG $ESNCONFIG --percent_dim
+
+# Run experiments with these parameters
+python -m scoop -n 8 ./esn_experiment.py $DATAFILE $ESNCONFIG $RUNS
